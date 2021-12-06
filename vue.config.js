16699490b1359993ab8bc8efd930223e50bfa0ff
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2021-12-06 16:21:48 -0300

    configuração do webpack

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule("svg");
+
+    svgRule.uses.clear();
+    svgRule
+      .test(/\.svg$/)
+      .use("svg-url-loader")
+      .loader("svg-url-loader");
+
+    config.module
+      .rule("images")
+      .use("url-loader")
+      .loader("url-loader")
+      .tap((options) => Object.assign(options, { limit: Infinity }));
+  },
+};
