8c037b0150458be0dd9bd987cc526335737b8e31
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2021-12-06 15:35:28 -0300

    reinstala tudo

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,3 @@
-const devPresets = ["@vue/babel-preset-app"];
-const buildPresets = [["@babel/preset-env"]];
-
 module.exports = {
-  presets: process.env.NODE_ENV === "development" ? devPresets : buildPresets
+  presets: ["@vue/cli-plugin-babel/preset"],
 };
--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  purge: ['./src/**/*.js', './src/**/*.vue'],
-  darkMode: false, // or 'media' or 'class'
-  presets: [require("@investfy/tailwind")],
-  plugins: [],
-}
