a057ea0c4c64bc298979f5098bc9f710c212bc6d
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2021-12-06 17:21:41 -0300

    componente de logotipo

--- src/components/logo/Logo.vue
+++ src/components/logo/Logo.vue
@@ -1,12 +1,25 @@
 <template>
-  <img src="./logo.svg" alt="Logotipo Investfy" />
+  <img :src="filename" alt="Logotipo Investfy" />
 </template>
 
 <script>
 export default {
   name: "Logo",
+
   props: {
-    type: String,
+    type: {
+      type: String,
+      default: "horizontal",
+      validator(str) {
+        return ["horizontal", "mini"].includes(str);
+      },
+    },
+  },
+
+  computed: {
+    filename() {
+      return require(`./logo-${this.type}.svg`);
+    },
   },
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,8 @@
 module.exports = {
   chainWebpack: (config) => {
     const svgRule = config.module.rule("svg");
-
     svgRule.uses.clear();
-    svgRule
-      .test(/\.svg$/)
-      .use("svg-url-loader")
-      .loader("svg-url-loader");
+    svgRule.use("svg-url-loader").loader("svg-url-loader");
 
     config.module
       .rule("images")
