23b843a807ec07dd6a6266ea5bae1565c3d3b342
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2021-12-07 18:58:00 -0300

    teste de logotipo atualizados

--- src/components/logo/Logo.spec.js
+++ src/components/logo/Logo.spec.js
@@ -1,36 +1,19 @@
-import { shallowMount } from "@vue/test-utils";
+import { render } from "@testing-library/vue";
 import IfyLogo from "./Logo.vue";
 
 describe("Logo.vue", () => {
   it("deve renderizar uma imagem", () => {
-    const wrapper = shallowMount(IfyLogo);
-    const list = wrapper.findAll("img");
-    expect(list.length).toBe(1);
-  });
-
-  describe.skip("o arquivo da imagem deve possuir a orientação correta", () => {
-    const orientation = "horizontal";
-    const color = "colored";
-
-    const wrapper = shallowMount(IfyLogo, {
-      propsData: {
-        orientation,
-        color,
-      },
-    });
-
-    const image = wrapper.find("img");
-    expect(image.attributes("src")).toBe(`./logo-${orientation}-${color}.svg`);
+    const { container } = render(IfyLogo);
+    expect(container.getElementsByTagName("img").length).toBe(1);
   });
 
   it("o texto alternativo da imagem deve renderizar corretamente", () => {
     const description = "Descrição do logotipo";
-    const wrapper = shallowMount(IfyLogo, {
-      propsData: { description },
+    const { getByAltText } = render(IfyLogo, {
+      props: { description },
     });
 
-    expect(wrapper.find("img").attributes("alt")).toEqual(description);
+    const image = getByAltText(description);
+    expect(image.getAttribute("alt")).toBe(description);
   });
-
-  it("o arquivo da imagem deve possuir a paleta de cores correta", () => {});
 });
