09a6f81ab432807a1e99b13e898bc391f0c2e328
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2021-12-07 11:02:24 -0300

    navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
-  <div id="app" class="container">
-    <div class="text-center">
-      <IfyLogo class="inline-block mb-6" />
+  <div id="app">
+    <IfyNavbar class="mb-8" />
+
+    <div class="container">
+      <IfyTable
+        :columns="[
+          { field: 'name', label: 'Nome' },
+          { field: 'email', label: 'E-mail' },
+        ]"
+        :data="[
+          { name: 'Isabel', email: 'isabel@example.com' },
+          { name: 'Lorenna', email: 'lorenna@example.com' },
+        ]"
+      />
     </div>
-    <IfyTable
-      :columns="[
-        { field: 'name', label: 'Nome' },
-        { field: 'email', label: 'E-mail' },
-      ]"
-      :data="[
-        { name: 'Isabel', email: 'isabel@example.com' },
-        { name: 'Lorenna', email: 'lorenna@example.com' },
-      ]"
-    />
   </div>
 </template>
 
@@ -21,14 +22,3 @@ export default {
   name: "App",
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
