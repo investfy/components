07d0abe5e87c03795387a4c34c73fa176e95bf0e
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2021-12-07 19:15:53 -0300

    instalação do fontawesome

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./assets/css/tailwind.css";
 import IfyComponents from "./index";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 Vue.use(IfyComponents);
+
+Vue.component("Fa", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
