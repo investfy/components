dfdfae283392b8ac88cd957c169820a73e55efa0
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2021-12-08 16:23:11 -0300

    avatar component

--- src/App.vue
+++ src/App.vue
@@ -6,27 +6,33 @@
           <template #trigger>
             <button
               type="button"
-              class="w-full px-4 py-2 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800"
+              class="flex items-center justify-between flex-shrink-0 focus:outline-none"
             >
-              <Fa :icon="ellipsisVIcon" />
+              <IfyAvatar
+                src="https://images.pexels.com/users/avatars/2417028/erik-mclean-965.jpeg?auto=compress&fit=crop&h=40&w=40"
+                title="John Doe"
+                class="mr-2"
+              />
+              <div class="flex flex-col items-stretch text-left">
+                <span
+                  class="hidden overflow-hidden text-sm overflow-ellipsis lg:block whitespace-nowrap"
+                >
+                  John Doe
+                </span>
+                <small class="hidden text-xs text-gray-600 lg:block">
+                  john.doe@example.com
+                </small>
+              </div>
             </button>
           </template>
 
-          <div class="w-56">
-            <div class="px-4 py-3">
-              <p class="text-sm leading-5">Conectado como</p>
-              <p class="text-sm font-medium leading-5 text-gray-900 truncate">
-                john.doe@example.com
-              </p>
-            </div>
-            <div class="py-1">
-              <IfyDropdownItem link="#">Configurações da conta</IfyDropdownItem>
-              <IfyDropdownItem link="#">Suporte</IfyDropdownItem>
-              <IfyDropdownItem>Novo recurso (em breve)</IfyDropdownItem>
-              <IfyDropdownItem link="#">Licença</IfyDropdownItem>
-              <IfyDropdownItem separator />
-              <IfyDropdownItem clickable>Sair</IfyDropdownItem>
-            </div>
+          <div class="w-52 py-1">
+            <IfyDropdownItem link="#">Configurações da conta</IfyDropdownItem>
+            <IfyDropdownItem link="#">Suporte</IfyDropdownItem>
+            <IfyDropdownItem>Novo recurso (em breve)</IfyDropdownItem>
+            <IfyDropdownItem link="#">Licença</IfyDropdownItem>
+            <IfyDropdownItem separator />
+            <IfyDropdownItem clickable>Sair</IfyDropdownItem>
           </div>
         </IfyDropdown>
       </template>
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+import IfyAvatar from "./avatar/Avatar.vue";
 import IfyDropdown from "./dropdown/Dropdown.vue";
 import IfyDropdownItem from "./dropdown/DropdownItem.vue";
 import IfyLogo from "./logo/Logo.vue";
@@ -5,6 +6,7 @@ import IfyNavbar from "./navbar/Navbar.vue";
 import IfyTable from "./table/Table.vue";
 
 const Components = {
+  IfyAvatar,
   IfyDropdown,
   IfyDropdownItem,
   IfyLogo,
