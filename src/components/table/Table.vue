3640fa2080c92c82b39b2d2aa594c4184e52b451
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2022-01-25 17:45:47 -0300

    abstração do componente de tabela

--- src/components/table/Table.vue
+++ src/components/table/Table.vue
@@ -1,43 +1,30 @@
 <template>
-  <table class="min-w-full divide-y divide-gray-200">
-    <thead v-if="columns.length" class="bg-gray-100">
-      <tr>
-        <th
-          v-for="(column, index) in columns"
-          :key="index"
-          class="px-6 py-3 text-left text-xs font-bold text-gray-600 uppercase tracking-wider"
-        >
-          {{ column.label }}
-        </th>
-      </tr>
-    </thead>
-
-    <tbody v-if="data.length" class="bg-white divide-y divide-gray-200">
-      <tr v-for="(row, index) in data" :key="index">
-        <template v-for="column in columns">
-          <td class="px-4 py-2 whitespace-nowrap" :key="column.field">
-            {{ row[column.field] || "" }}
-          </td>
-        </template>
-      </tr>
-    </tbody>
-  </table>
+  <VueGoodTable v-bind="$props" v-on="$listeners">
+    <slot v-for="(_, name) in $slots" :name="name" :slot="name" />
+    <template
+      v-for="(_, name) in $scopedSlots"
+      :slot="name"
+      slot-scope="slotData"
+      ><slot :name="name" v-bind="slotData"
+    /></template>
+  </VueGoodTable>
 </template>
 
 <script>
+import "vue-good-table/dist/vue-good-table.css";
+import { VueGoodTable } from "vue-good-table";
+
 export default {
   name: "IfyTable",
 
-  props: {
-    data: {
-      type: Array,
-      default: () => [],
-    },
-
-    columns: {
-      type: Array,
-      default: () => [],
-    },
+  components: {
+    VueGoodTable,
   },
+
+  props: Object.assign(VueGoodTable.props, {
+    styleClass: {
+      default: "vgt-table striped bordered",
+    },
+  }),
 };
 </script>
