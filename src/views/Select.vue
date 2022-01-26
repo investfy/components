aeca95c2be9f1db459b19088519c778e7f5efe49
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2022-01-26 16:31:12 -0300

    feat(IfyTable): Aprimoramento do componente
    Refatoração do componente `IfyTable` para usar o componente da biblioteca [`vue-good-table`](https://xaksis.github.io/vue-good-table/), que possui diversas abstrações e features já implementadas.

--- src/views/Select.vue
+++ src/views/Select.vue
@@ -1,90 +1,171 @@
 <template>
-  <div
-    class="control"
-    :class="{ 'is-expanded': expanded, 'has-icons-left': icon }"
-  >
-    <span class="select" :class="spanClasses">
-      <select
-        v-model="computedValue"
-        ref="select"
-        :multiple="multiple"
-        :size="nativeSize"
-        v-bind="$attrs"
-        @blur="$emit('blur', $event) && checkHtml5Validity()"
-        @focus="$emit('focus', $event)"
-      >
-        <template v-if="placeholder">
-          <option v-if="computedValue == null" :value="null" disabled hidden>
-            {{ placeholder }}
+  <div class="my-8 space-y-4">
+    <section>
+      <IfyField label="Simple">
+        <IfySelect placeholder="Select a name">
+          <option v-for="option in data" :value="option.id" :key="option.id">
+            {{ option.user.first_name }}
           </option>
-        </template>
+        </IfySelect>
+      </IfyField>
 
-        <slot />
-      </select>
-    </span>
+      <IfyField label="Grouped">
+        <IfySelect placeholder="Select a character" icon="user">
+          <optgroup label="Black Sails">
+            <option value="flint">Flint</option>
+            <option value="silver">Silver</option>
+            <option value="vane">Vane</option>
+            <option value="billy">Billy</option>
+            <option value="jack">Jack</option>
+          </optgroup>
 
-    <IfyIcon v-if="icon" class="is-left" :icon="icon" :size="iconSize" />
+          <optgroup label="Breaking Bad">
+            <option value="heisenberg">Heisenberg</option>
+            <option value="jesse">Jesse</option>
+            <option value="saul">Saul</option>
+            <option value="mike">Mike</option>
+          </optgroup>
+
+          <optgroup label="Game of Thrones">
+            <option value="tyrion-lannister">Tyrion Lannister</option>
+            <option value="jamie-lannister">Jamie Lannister</option>
+            <option value="daenerys-targaryen">Daenerys Targaryen</option>
+            <option value="jon-snow">Jon Snow</option>
+          </optgroup>
+        </IfySelect>
+      </IfyField>
+
+      <IfyField
+        label="Error"
+        type="is-danger"
+        message="Something went wrong with this field"
+      >
+        <IfySelect placeholder="Select a character">
+          <option value="flint">Flint</option>
+          <option value="silver">Silver</option>
+        </IfySelect>
+      </IfyField>
+
+      <IfyField label="Rounded">
+        <IfySelect placeholder="Select a character" rounded>
+          <option value="flint">Flint</option>
+          <option value="silver">Silver</option>
+        </IfySelect>
+      </IfyField>
+
+      <IfyField label="Required">
+        <IfySelect placeholder="Select a character" required>
+          <option value="flint">Flint</option>
+          <option value="silver">Silver</option>
+        </IfySelect>
+      </IfyField>
+
+      <IfyField label="Loading">
+        <IfySelect placeholder="Select a character" loading>
+          <option value="flint">Flint</option>
+          <option value="silver">Silver</option>
+        </IfySelect>
+      </IfyField>
+
+      <IfyField label="Disabled">
+        <IfySelect placeholder="Select a character" disabled>
+          <option value="flint">Flint</option>
+          <option value="silver">Silver</option>
+        </IfySelect>
+      </IfyField>
+
+      <IfyField label="Disabled option">
+        <IfySelect placeholder="Select a character">
+          <option value="flint">Flint</option>
+          <option value="silver" disabled>Silver</option>
+        </IfySelect>
+      </IfyField>
+
+      <IfyField label="Expanded">
+        <IfySelect placeholder="Select a character" expanded>
+          <option value="flint">Flint</option>
+          <option value="silver">Silver</option>
+        </IfySelect>
+      </IfyField>
+    </section>
+
+    <section>
+      <h2 class="title">Multiple</h2>
+      <p class="content"><b>selected</b>: {{ selectedOptions }}</p>
+      <IfyField>
+        <IfySelect multiple native-size="8" v-model="selectedOptions">
+          <option value="flint">Flint</option>
+          <option value="silver">Silver</option>
+          <option value="vane">Vane</option>
+          <option value="billy">Billy</option>
+          <option value="jack">Jack</option>
+          <option value="heisenberg">Heisenberg</option>
+          <option value="jesse">Jesse</option>
+          <option value="saul">Saul</option>
+          <option value="mike">Mike</option>
+        </IfySelect>
+      </IfyField>
+    </section>
+
+    <section>
+      <h2 class="title">Icons</h2>
+      <IfyField>
+        <IfySelect placeholder="Country" icon="globe">
+          <option value="1">Option 1</option>
+          <option value="2">Option 2</option>
+        </IfySelect>
+      </IfyField>
+
+      <IfyField>
+        <IfySelect placeholder="Client" icon="user">
+          <option value="1">Option 1</option>
+          <option value="2">Option 2</option>
+        </IfySelect>
+      </IfyField>
+    </section>
+
+    <section>
+      <h2 class="title">Sizes</h2>
+      <IfyField>
+        <IfySelect placeholder="Small" size="is-small" expanded>
+          <option value="flint">Flint</option>
+          <option value="silver">Silver</option>
+        </IfySelect>
+      </IfyField>
+
+      <IfyField>
+        <IfySelect placeholder="Default" expanded>
+          <option value="flint">Flint</option>
+          <option value="silver">Silver</option>
+        </IfySelect>
+      </IfyField>
+
+      <IfyField>
+        <IfySelect placeholder="Medium" size="is-medium" expanded>
+          <option value="flint">Flint</option>
+          <option value="silver">Silver</option>
+        </IfySelect>
+      </IfyField>
+
+      <IfyField>
+        <IfySelect placeholder="Large" size="is-large" expanded>
+          <option value="flint">Flint</option>
+          <option value="silver">Silver</option>
+        </IfySelect>
+      </IfyField>
+    </section>
   </div>
 </template>
 
 <script>
-import FormElementMixin from "../../utils/FormElementMixin";
+const data = require("@/data/sample.json");
 
 export default {
-  name: "IfySelect",
-
-  mixins: [FormElementMixin],
-
-  inheritAttrs: false,
-
-  props: {
-    value: {
-      type: [String, Number, Boolean, Object, Array, Function, Date],
-      default: null,
-    },
-    placeholder: String,
-    multiple: Boolean,
-    nativeSize: [String, Number],
-  },
-
   data() {
     return {
-      selected: this.value,
-      elementRef: "select",
+      data,
+      selectedOptions: [],
     };
-  },
-
-  computed: {
-    computedValue: {
-      get() {
-        return this.selected;
-      },
-      set(value) {
-        this.selected = value;
-        this.$emit("input", value);
-        !this.isValid && this.checkHtml5Validity();
-      },
-    },
-    spanClasses() {
-      return [
-        this.size ? `is-${this.size}` : null,
-        this.statusType ? `is-${this.statusType}` : null,
-        {
-          "is-fullwidth": this.expanded,
-          "is-loading": this.loading,
-          "is-multiple": this.multiple,
-          "is-rounded": this.rounded,
-          "is-empty": this.selected === null,
-        },
-      ];
-    },
-  },
-
-  watch: {
-    value(value) {
-      this.selected = value;
-      !this.isValid && this.checkHtml5Validity();
-    },
   },
 };
 </script>
--- src/views/Table.vue
+++ src/views/Table.vue
@@ -1,19 +1,46 @@
 <template>
-  <section>
-    <h2 class="title">IfyTable</h2>
-    <IfyTable
-      :columns="[
-        { field: 'name', label: 'Nome' },
-        { field: 'email', label: 'E-mail' },
-      ]"
-      :data="[
-        { name: 'Isabel', email: 'isabel@example.com' },
-        { name: 'Lorenna', email: 'lorenna@example.com' },
-      ]"
-    />
+  <section class="my-8">
+    <IfyTable :columns="columns" :rows="rows" compact-mode />
   </section>
 </template>
 
 <script>
-export default { name: "Table" };
+const rows = require("@/data/sample.json");
+
+export default {
+  name: "Table",
+
+  computed: {
+    columns() {
+      return [
+        {
+          field: "id",
+          label: "ID",
+          width: "40",
+          numeric: true,
+        },
+        {
+          field: "user.first_name",
+          label: "First Name",
+        },
+        {
+          field: "user.last_name",
+          label: "Last Name",
+        },
+        {
+          field: "date",
+          label: "Date",
+          centered: true,
+        },
+        {
+          field: "gender",
+          label: "Gender",
+        },
+      ];
+    },
+    rows() {
+      return rows;
+    },
+  },
+};
 </script>
