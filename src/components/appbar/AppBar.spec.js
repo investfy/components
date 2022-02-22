9d7d60375c4cf281f244f36148962e40d1d1629d
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2022-02-22 16:15:57 -0300

    refatoração dos testes

--- src/components/appbar/AppBar.spec.js
+++ src/components/appbar/AppBar.spec.js
@@ -1,113 +1,63 @@
-import { shallowMount, mount, RouterLinkStub } from "@vue/test-utils";
-import AppBar from "./AppBar.vue";
+import { shallowMount } from "@vue/test-utils";
+import IfyAppBar from "./AppBar.vue";
 
-describe("AppBar.vue", () => {
-  it("deve possuir o link", () => {
-    const brandLink = "/forum";
-    const wrapper = shallowMount(AppBar, {
+describe("IfyAppBar", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(IfyAppBar, {
       stubs: {
-        RouterLink: RouterLinkStub,
-      },
-      propsData: {
-        brandLink,
+        IfyLogo: { template: '<img class="logotipo" />' },
+        RouterLink: { template: '<a class="router-link"><slot /></a>' },
       },
     });
-    expect(wrapper.findComponent(RouterLinkStub).props().to).toBe(brandLink);
   });
 
-  it("slot #start deve estar centralizado", () => {
-    const centered = true;
-
-    const wrapper = shallowMount(AppBar, {
-      propsData: {
-        centered,
-      },
-      slots: {
-        start: '<div class="fake-msg"></div>',
-      },
-    });
-
-    const startSlot = wrapper.find(".start-slot");
-    expect(startSlot.classes().includes("is-centered")).toBe(true);
+  it("pode ter um link associado ao logotipo", async () => {
+    await wrapper.setProps({ brandLink: "/forum" });
+    expect(wrapper.find(".router-link").attributes("to")).toBe("/forum");
   });
 
-  it("o start slot não deve estar centralizado", () => {
-    const centered = false;
-
-    const wrapper = shallowMount(AppBar, {
-      propsData: {
-        centered,
-      },
-      slots: {
-        start: '<div class="fake-msg"></div>',
-      },
-    });
-
-    const startSlot = wrapper.find(".start-slot");
-    expect(startSlot.classes().includes("is-centered")).toBe(false);
+  it("pode ter o conteúdo central centralizado", async () => {
+    await wrapper.setProps({ centered: true });
+    expect(wrapper.findAll(".is-centered").length).toBe(3);
   });
 
-  it("deve possuir classe container", () => {
-    const full = false;
-
-    const wrapper = shallowMount(AppBar, {
-      propsData: {
-        full,
-      },
-    });
-    expect(wrapper.find(".container").exists()).toBe(true);
+  it("pode se comportar de maneira responsiva", async () => {
+    await wrapper.setProps({ full: true });
+    expect(wrapper.find("header > .container").exists()).toBeFalsy();
   });
 
-  it("não deve possuir classe container", () => {
-    const full = true;
-
-    const wrapper = shallowMount(AppBar, {
-      propsData: {
-        full,
-      },
-    });
-    expect(wrapper.find(".container").exists()).toBe(false);
+  it("pode ter a sombra desabilitada", async () => {
+    await wrapper.setProps({ shadow: false });
+    expect(wrapper.find("header.shadow-md").exists()).toBeFalsy();
   });
 
-  it("deve possuir classe shadow-md", () => {
-    const shadow = true;
-
-    const wrapper = shallowMount(AppBar, {
-      propsData: {
-        shadow,
-      },
-    });
-    expect(wrapper.find(".shadow-md").exists()).toBe(true);
+  it("pode ter o fundo transparente", async () => {
+    await wrapper.setProps({ transparent: true });
+    expect(wrapper.find("header.bg-white").exists()).toBeFalsy();
   });
 
-  it("não deve possuir classe shadow-md", () => {
-    const shadow = false;
-    const wrapper = shallowMount(AppBar, {
-      propsData: {
-        shadow,
-      },
+  it("pode ter o logotipo substituído", () => {
+    expect(wrapper.find("img.logotipo").exists()).toBeTruthy();
+    const localWrapper = shallowMount(IfyAppBar, {
+      slots: { brand: "Investfy" },
     });
-    expect(wrapper.find(".shadow-md").exists()).toBe(false);
+    expect(localWrapper.find("img.logotipo").exists()).toBeFalsy();
+    expect(localWrapper.find(".brand-slot").text()).toBe("Investfy");
   });
 
-  it("Não deve possuir classe bg-white", () => {
-    const transparent = true;
-
-    const wrapper = shallowMount(AppBar, {
-      propsData: {
-        transparent,
-      },
+  it("deve exibir conteúdo ao lado do logotipo", () => {
+    const localWrapper = shallowMount(IfyAppBar, {
+      slots: { start: "Investfy" },
     });
-    expect(wrapper.find(".bg-white").exists()).toBe(false);
+    expect(localWrapper.find(".start-slot").text()).toBe("Investfy");
   });
 
-  test("não deve possuir classe bg-white", () => {
-    const transparent = false;
-    const wrapper = mount(AppBar, {
-      propsData: {
-        transparent,
-      },
+  it("deve exibir conteúdo no lado direito", () => {
+    const localWrapper = shallowMount(IfyAppBar, {
+      slots: { end: "Investfy" },
     });
-    expect(wrapper.find(".bg-white").exists()).toBe(true);
+    expect(localWrapper.find(".end-slot").text()).toBe("Investfy");
   });
 });
