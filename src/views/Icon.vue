4b1292ac090053a0307d9ca4982f966eb7d58741
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2022-02-22 15:30:10 -0300

    página de documentação

--- src/views/Icon.vue
+++ src/views/Icon.vue
@@ -1,45 +1,25 @@
 <template>
-  <section>
-    <h2 class="title">IfyIcon</h2>
-    <div class="space-y-2">
-      <div>
-        <IfyIcon icon="user-circle" size="xs" />
-        <IfyIcon icon="home" size="xs" />
-        <IfyIcon icon="tachometer-alt" size="xs" />
-      </div>
-      <div>
-        <IfyIcon icon="user-circle" size="sm" />
-        <IfyIcon icon="home" size="sm" />
-        <IfyIcon icon="tachometer-alt" size="sm" />
-      </div>
-
-      <div>
-        <IfyIcon icon="user-circle" />
-        <IfyIcon icon="home" />
-        <IfyIcon icon="tachometer-alt" />
-      </div>
-
-      <div>
-        <IfyIcon icon="user-circle" size="lg" />
-        <IfyIcon icon="home" size="lg" />
-        <IfyIcon icon="tachometer-alt" size="lg" />
-      </div>
-
-      <div>
-        <IfyIcon icon="user-circle" size="2x" />
-        <IfyIcon icon="home" size="2x" type="success" />
-        <IfyIcon icon="tachometer-alt" size="2x" type="warning" />
-      </div>
-
-      <div>
-        <IfyIcon icon="user-circle" size="3x" type="danger" />
-        <IfyIcon icon="home" size="3x" type="info" />
-        <IfyIcon icon="tachometer-alt" size="3x" type="primary" />
-      </div>
+  <div class="space-y-4">
+    <div v-for="type in types" :key="type">
+      <span v-for="size in sizes" :key="size">
+        <IfyIcon icon="camera" :type="type" :size="size" />
+        &nbsp;
+      </span>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
-export default { name: "Icon" };
+export default {
+  name: "Icon",
+
+  computed: {
+    types() {
+      return ["primary", "success", "warning", "danger", "info"];
+    },
+    sizes() {
+      return ["xs", "sm", "md", "lg", "xl", "2xl", "3xl", "4xl", "5xl"];
+    },
+  },
+};
 </script>
