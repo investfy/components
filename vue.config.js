aec70fd9e0fd8fabc38522f0455db0d94ec1dec1
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2022-02-24 11:17:58 -0300

    config do path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/components/" : "/",
   transpileDependencies: true,
   chainWebpack(config) {
     const svgRule = config.module.rule("svg");
