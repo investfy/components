cb19e4e0aee893f626f0973abe01b49689881026
vctrtvfrrr <victor@otavioferreira.com.br>
2022-02-25 19:00:51 +0000

    Deploying to gh-pages from @ investfy/components@d9af9dc57d4100ec1c2286236ead05644a8885d1 🚀

--- js/184.fdc1196c.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunk_investfy_components"]=self["webpackChunk_investfy_components"]||[]).push([[184],{184:function(n,e,t){t.r(e),t.d(e,{default:function(){return f}});var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div")},s=[],l={name:"Logo"},c=l,o=t(1001),r=(0,o.Z)(c,u,s,!1,null,null,null),f=r.exports}}]);
-//# sourceMappingURL=184.fdc1196c.js.map
