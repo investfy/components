d9af9dc57d4100ec1c2286236ead05644a8885d1
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2022-02-25 15:58:49 -0300

    fix: Empacotamento dos assets pelo Webpack
    Correção das configurações do Webpack que impediam que os arquivos SVG fossem inseridos no bundle final do pacote.

--- src/views/Logo.vue
+++ src/views/Logo.vue
@@ -1,4 +1,47 @@
-<template><div></div></template>
+<template>
+  <section>
+    <div
+      class="bg-gray-200 flex justify-center items-center w-1/2 mx-auto mb-4 p-4 text-center"
+    >
+      <IfyLogo
+        :orientation="orientation"
+        :color="color"
+        :description="description"
+      />
+    </div>
+
+    <IfyField label="orientation:">
+      <IfySelect v-model="orientation">
+        <option value="horizontal" selected>horizontal</option>
+        <option value="mini">mini</option>
+      </IfySelect>
+    </IfyField>
+
+    <IfyField label="color:">
+      <IfySelect v-model="color">
+        <option value="colored" selected>colored</option>
+        <option value="inverted">inverted</option>
+        <option value="white">white</option>
+        <option value="black">black</option>
+      </IfySelect>
+    </IfyField>
+
+    <IfyField label="description:">
+      <IfyInput v-model="description" type="textarea" rows="2" />
+    </IfyField>
+  </section>
+</template>
+
 <script>
-export default { name: "Logo" };
+export default {
+  name: "Logo",
+
+  data() {
+    return {
+      orientation: "horizontal",
+      color: "colored",
+      description: "Logotipo Investfy",
+    };
+  },
+};
 </script>
--- vue.config.js
+++ vue.config.js
@@ -3,15 +3,15 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === "production" ? "/components/" : "/",
   transpileDependencies: true,
-  chainWebpack(config) {
+  chainWebpack: (config) => {
     const svgRule = config.module.rule("svg");
     svgRule.uses.clear();
-    svgRule.use("svg-url-loader").loader("svg-url-loader");
+    svgRule.set("parser", {
+      dataUrlCondition: { maxSize: Infinity },
+    });
 
-    config.module
-      .rule("images")
-      .use("url-loader")
-      .loader("url-loader")
-      .tap((options = {}) => Object.assign(options, { limit: Infinity }));
+    config.module.rule("images").set("parser", {
+      dataUrlCondition: { maxSize: Infinity },
+    });
   },
 });
