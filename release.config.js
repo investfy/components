4d84fbc8bbddc5b5b4aeae5e4ccc643376db5649
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2022-03-02 21:57:53 -0300

    fix: Script de CI do Github Actions

--- release.config.js
+++ release.config.js
@@ -3,18 +3,7 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/npm",
-    [
-      "@semantic-release/github",
-      {
-        assets: [
-          "dist",
-          "docs",
-          "package.json",
-          "package-lock.json",
-          "README.md",
-        ],
-      },
-    ],
+    "@semantic-release/github",
     [
       "@semantic-release/git",
       { assets: ["dist", "docs", "package.json", "package-lock.json"] },
