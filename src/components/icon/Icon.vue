3946c6e65bdbeb48980a11091689f8719df1fd39
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2022-03-11 14:16:54 -0300

    fix(Icon): Tamanhos do permitidos

--- src/components/icon/Icon.vue
+++ src/components/icon/Icon.vue
@@ -17,7 +17,17 @@ export default {
       type: String,
       default: null,
       validator(str) {
-        return ["xs", "sm", "md", "lg", "xl"].includes(str);
+        return [
+          "xs",
+          "sm",
+          "md",
+          "lg",
+          "xl",
+          "2xl",
+          "3xl",
+          "4xl",
+          "5xl",
+        ].includes(str);
       },
     },
     customClass: {
@@ -28,20 +38,26 @@ export default {
 
   computed: {
     computedSize() {
-      if (this.size === "md") return null;
-      if (this.size === "xl") return "2x";
-      if (this.size === "2xl") return "3x";
-      if (this.size === "3xl") return "5x";
-      if (this.size === "4xl") return "7x";
-      if (this.size === "5xl") return "10x";
-      return this.size;
+      const sizes = {
+        xs: "xs",
+        sm: "sm",
+        md: null,
+        lg: "lg",
+        xl: "2x",
+        "2xl": "3x",
+        "3xl": "5x",
+        "4xl": "7x",
+        "5xl": "10x",
+      };
+
+      return sizes[this.size];
     },
   },
 };
 </script>
 
-<style lang="postcss" scoped>
+<style lang="postcss">
 .ifyicon {
-  @apply select-none inline-block items-center justify-center;
+  @apply select-none inline-block;
 }
 </style>
